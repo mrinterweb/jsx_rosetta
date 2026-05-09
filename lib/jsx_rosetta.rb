4fcc99dc1492d463d0a36500d934b75b7b2886f6
Sean McCleary <seanmcc@gmail.com>
2026-05-09 09:58:54 -0700

    Initial generated gem

---
+++ lib/jsx_rosetta.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jsx_rosetta/version"
+
+module JsxRosetta
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jsx_rosetta/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JsxRosetta
+  VERSION = "0.1.0"
+end
